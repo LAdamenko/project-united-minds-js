e7b38cc963ae40d715577d9b60e04ef7bebd4f4f
Ladamenko <ladamenko@gmail.com>
2024-04-10 17:17:49 +0300

    clear repo

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './js/review';
+import './js/footer';
+import './js/about';
+import './js/api';
+import './js/benefits';
+import './js/covers';
+import './js/faq';
+import './js/header';
+import './js/hero';
+import './js/modal';
+import './js/projects';
+import './js/slider';
