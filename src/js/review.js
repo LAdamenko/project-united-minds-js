f519d407a2133e26b09a9967f49962cc428583db
Orest-Stetsyk <62755155+Orest-Stetsyk@users.noreply.github.com>
2024-04-11 15:49:48 +0300

    add render function and swiper on reviews

--- src/js/review.js
+++ src/js/review.js
@@ -4,11 +4,13 @@ import iziToast from "izitoast";
 
 import "izitoast/dist/css/iziToast.min.css";
 import 'swiper/css';
+import { Navigation, Keyboard,Mousewheel } from 'swiper/modules';
+import { apiGet } from './api';
 
 
 const notFoundMessage = document.querySelector('.not-found');
 const reviewsList = document.querySelector(`.reviews-list`);
-const swiperBlock = document.querySelector(`.swiper`)    
+const swiperBlock = document.querySelector(`.swiper-container`)    
 
 function displayToast(message) {
   iziToast.error({
@@ -18,50 +20,78 @@ function displayToast(message) {
   });
 }
 
-
-document.addEventListener('DOMContentLoaded', function () {
-    const swiper = new Swiper('.swiper', {
-        slidesPerView: 1,
-        breakpoints: {
-            768: {
-                slidesPerView: 2,
-            },
-            1440: {
-                slidesPerView: 4,
-            },
-            navigation: {
-                nextEl: '.next-button',
-                prevEl: '.prev-button',
-            },
-        }
-    });
-    
-function render(data) {
-  if (data.hits.length === 0) {
-      displayToast("Sorry, there are no images matching your search query. Please try again!");
-      swiperHide();
-      notFoundShow()
+function render(reviews) {
+  if (reviews.length === 0) {
+    reviewsIsNotFounded();
   }
   else {
-    const reviews = data.hits;
-
-      const markup = reviews.map(review => `<li class="reviews-item">
-                <img src="" alt="" width="48" height="48"  class="reviews-avatar"/>
+    const markup = reviews.map(review => `<li class="reviews-item swiper-slide">
+                <img src="${review.avatar_url}" alt="" width="48" height="48"  class="reviews-avatar"/>
                 <div class="reviews-info">
-                    <h3 class="reviews-name"></h3>
-                    <p class="reviews-text"></p>
+                    <h3 class="reviews-name">${review.author}</h3>
+                    <p class="reviews-text">${review.review}</p>
                 </div>
               </li>`)
       .join('');
     reviewsList.insertAdjacentHTML('beforeend', markup);
+    initializeSwiper();
   }
 }
-});
 
-function swiperHide(){
-swiperBlock.style.display = 'none';
+function reviewsIsNotFounded() {
+  swiperBlock.style.display = 'none';
+  notFoundMessage.style.display = 'block';
+  displayToast(`The connection with the server has problem, so reviews are not rendered`)
 }
 
-function notFoundShow() {
-    notFoundMessage.style.display = 'block'
+function checkAndRenderReviews() {
+  apiGet().then(data => {
+    if (data && data.length > 0) {
+      render(data);
+      
+    } else {
+      reviewsIsNotFounded();
+    }
+  }).catch(error => {
+     console.error(error);
+    reviewsIsNotFounded();
+  })
 }
+
+function initializeSwiper() {
+  const swiperReviews = new Swiper('.swiper-container', {
+    modules: [Navigation, Keyboard, Mousewheel],
+    slidesPerView: 1,
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+       
+      },
+      1440: {
+        slidesPerView: 4,
+       
+      },
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+    mousewheel: {enabled: true,
+      },
+    speed: 800,
+  spaceBetween: 16,
+  allowTouchMove: true,
+    loop: false,
+  watchOverflow: true,
+  slideToClickedSlide: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+}
+
+document.addEventListener('DOMContentLoaded', checkAndRenderReviews);
+
+
+
