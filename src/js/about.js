cfe2c5af8f5a9ef1f25155a464dad783c988324b
MariannaHon <imarianna96i@gmail.com>
2024-04-11 21:37:21 +0200

    About-trabls

--- src/js/about.js
+++ src/js/about.js
@@ -5,12 +5,12 @@ import Swiper from 'swiper';
 import 'swiper/css';
 import { Keyboard, Mousewheel } from 'swiper/modules';
 
-// const buttons = document.querySelectorAll('.btn-about');
+const buttons = document.querySelectorAll('.btn-about');
 
-// const acc = new Accordion('.accordion-container', {
-//   showMultiple: true,
-//   collapse: true,
-// });
+const acc = new Accordion('.accordion-container', {
+  showMultiple: true,
+  collapse: true,
+});
 
 // const icon = document.querySelector('.icon-about');
 
@@ -20,53 +20,74 @@ import { Keyboard, Mousewheel } from 'swiper/modules';
 //   console.log('hello');
 // }
 
+document.addEventListener("DOMContentLoaded", () => {
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      acc.open();
+      console.log('hello');
+    });
+  });
+}
 
-// buttons.forEach((button) => {
-//   button.addEventListener("click", () => {
-//     acc.open();
-//     switcher();
+
+// const acc = new Accordion('.accordion-container', {
+//   showMultiple: true,
+// });
+
+// const accordionItems = document.querySelectorAll('.ac');
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   accordionItems.forEach(item => {
+//     const header = item.querySelector('.ac-header');
+//     const button = item.querySelectorAll('.btn-about');
+//     const content = item.querySelector('.ac-panel');
+
+//     header.addEventListener('click', () => {
+//       if (content.classList.contains('text-cv-about-container')) {
+//         accordion.close(content);
+//       } else {
+//         button.click();
+//       }
+//     });
 //   });
 // });
 
-// buttons.forEach(function (button) {
-//   button.addEventListener("click", () => {
-//     console.log('hello');
-//   });
-// })
 
 
-// const swiper = new Swiper('.swiper-about', {
-//   modules: [Keyboard, Mousewheel],
-//   speed: 400,
-//   spaceBetween: 0,
-//   allowTouchMove: true,
-//   loop: true,
-//   slideToClickedSlide: true,
-//   keyboard: {
-//     enabled: true,
-//     onlyInViewport: true,
-//   },
-//   mousewheel: {
-//     enabled: true,
-//   },
-//   slidesPerView: 'auto',
-//   breakpoints: {
-//     375: {
-//       slidesPerView: 2
-//     },
-//     768: {
-//       slidesPerView: 3
-//     },
-//     1440: {
-//       slidesPerView: 6
-//     }
-//   }
-// });
 
-// const swipeBtn = document.querySelector('.btn-arrow');
 
-// swipeBtn.addEventListener("click", () => {
-//   swiper.slideNext();
-//   swiper.updateSize();
-//   swiper.updateSlides();
-// });
+
+
+const swiper = new Swiper('.swiper-about', {
+  modules: [Keyboard, Mousewheel],
+  speed: 400,
+  spaceBetween: 0,
+  allowTouchMove: true,
+  loop: true,
+  slideToClickedSlide: true,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  mousewheel: {
+    enabled: true,
+  },
+  slidesPerView: 2,
+  breakpoints: {
+    768: {
+      slidesPerView: 3
+    },
+    1440: {
+      slidesPerView: 6
+    }
+  }
+});
+
+const swipeBtn = document.querySelector('.btn-arrow');
+
+swipeBtn.addEventListener("click", () => {
+  swiper.slideNext();
+  swiper.updateSize();
+  swiper.updateSlides();
+});
