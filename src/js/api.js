63c7b804804ca0ef60a01f8c54c6b9f84afe64d1
AnnaSofiya1 <annasofiya1111@gmail.com>
2024-04-11 11:50:26 +0300

    apiPost

--- src/js/api.js
+++ src/js/api.js
@@ -12,3 +12,16 @@ export function apiGet(){
             throw error;
         });
 }
+
+export function apiPost(data){
+    const base_url = "https://portfolio-js.b.goit.study";
+    const end_point = "/api/footer";
+    const url = `${base_url}${end_point}`;
+
+    return axios.post(url, data)
+        .then(response => response.data)
+        .catch(error => {
+            console.error(error);
+            throw error;
+        });
+}
