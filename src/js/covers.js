5f7e62b25e5f99d53dab459830d97ecdc7ab33b3
Oleh Liubimov <mtbs1996@gmail.com>
2024-04-12 10:13:49 +0300

    Update covers.js

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,33 +1,37 @@
 
 const coverSection = document.querySelector(".covers-section");
+// Об'єкт налаштувань для Observer'а.
 const options = {
     root: null,
     rootMargin: "270px",
     threshold: 0.5,
 }
+// Колбек функція для Observer'а.
 const observeFunc = (enteries) => {
+    // Observer створює масив входженнь таргета в зону спостереждення
     enteries.forEach(entry => {
+        // Коли таргет входить в зону спостереження запускаємо анімацію
       if (entry.isIntersecting) {
           startAnimation();
+        //   Коли виходить анімацію зупиняємо
       } else {
           stopAnimation();
       }
     });
 }
-
+// Функція зупинки анімації
 function stopAnimation() {
     const slideItem = document.querySelectorAll(".covers-slide");
     slideItem.forEach((slide) => slide.classList.remove("animation"))
 }
+// Функція запуску анімації
 function startAnimation() {
     const slideItem = document.querySelectorAll(".covers-slide");
     slideItem.forEach((slide) => slide.classList.add("animation"));
     
 }
-
-
-
-
+// Створення екземпляру Interseption Observer 
 const observer = new IntersectionObserver(observeFunc, options);
+// Передаємо секцію в якості таргета для спостереження 
 observer.observe(coverSection)
 
