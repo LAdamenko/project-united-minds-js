8973195381086813a359c4bd3e33b76aade12914
MariannaHon <imarianna96i@gmail.com>
2024-04-12 11:52:45 +0200

    commit

--- src/js/about.js
+++ src/js/about.js
@@ -1,79 +1,43 @@
 import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
-
-
 
 import Swiper from 'swiper';
 import 'swiper/css';
 import { Keyboard, Mousewheel } from 'swiper/modules';
 
-const buttons = document.querySelectorAll('.btn-about');
-
-const acc = new Accordion('.accordion-container', {
-  showMultiple: true,
-});
-
 const icons = document.querySelectorAll('.icon-about');
 
-function switcher() {
-  icons.forEach((i) => {
-    i.classList.toggle('icon-close');
-    i.classList.toggle('icon-open');
-  });
-}
+const buttons = document.querySelectorAll('.btn-about');
+
+const accordionItems = document.querySelectorAll('.ac');
+
 
 document.addEventListener("DOMContentLoaded", () => {
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const content = document.querySelector('.ac-panel');
-      icons.forEach((icon) => {
-        if (icon.classList.contains('icon-open')) {
-          acc.close(content);
-        }
-        else {
-          acc.open(content);
-        }
-      })
-      switcher();
-      console.log('hello');
+  const acc = new Accordion('.accordion-container', {
+    showMultiple: true,
+    openOnInit: [0],
+  });
+  accordionItems.forEach(item => {
+    const header = item.querySelector('.ac-header');
+    const panel = item.querySelector('.ac-panel');
+
+    header.addEventListener('click', () => {
+      const isActive = panel.classList.contains('active');
+      if (isActive) {
+        acc.close(panel);
+      } else {
+        acc.open(panel);
+      }
     });
   });
+  buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const icon = event.currentTarget.querySelector('.icon-about');
+      icon.classList.toggle('icon-close');
+      icon.classList.toggle('icon-open');
+    });
+  });
+
 });
-
-
-
-
-
-
-
-
-// const acc = new Accordion('.accordion-container', {
-//   showMultiple: true,
-// });
-
-// const accordionItems = document.querySelectorAll('.ac');
-
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   accordionItems.forEach(item => {
-//     const header = item.querySelector('.ac-header');
-//     const button = item.querySelectorAll('.btn-about');
-//     const content = item.querySelector('.ac-panel');
-
-//     header.addEventListener('click', () => {
-//       if (content.classList.contains('text-cv-about-container')) {
-//         accordion.close(content);
-//       } else {
-//         button.click();
-//       }
-//     });
-//   });
-// });
-
-
-
-
-
 
 
 const swiper = new Swiper('.swiper-about', {
@@ -91,12 +55,15 @@ const swiper = new Swiper('.swiper-about', {
     enabled: true,
   },
   slidesPerView: 2,
+  width: 260,
   breakpoints: {
     768: {
-      slidesPerView: 3
+      slidesPerView: 3,
+      width: 600
     },
     1440: {
-      slidesPerView: 6
+      slidesPerView: 6,
+      width: 1200
     }
   }
 });
