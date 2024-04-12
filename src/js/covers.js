3ce72b05f3f267fe4a329186d2716d36f12e4ae1
Oleh Liubimov <mtbs1996@gmail.com>
2024-04-12 09:57:37 +0300

    js fix, images?

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,40 +1,28 @@
-// const observer = new IntersectionObserver((enteries) => {
-    //     enteries.forEach(entery => {
-//         if (entery.isIntersecting) {
-//             startAnimation();
-//         } else {
-    //             stopAnimation();
-    //         }
-    //     })
-    // });
-    // observer.observe(coverWrapper);
+
 const coverSection = document.querySelector(".covers-section");
 const options = {
     root: null,
-    rootMargin: "0px",
+    rootMargin: "270px",
     threshold: 0.5,
 }
-const observeFunc = (enteries, observer) => {
+const observeFunc = (enteries) => {
     enteries.forEach(entry => {
       if (entry.isIntersecting) {
           startAnimation();
-           console.log('Анімація працює');
       } else {
           stopAnimation();
-           console.log('Анімація зупинена');
       }
     });
 }
 
 function stopAnimation() {
-    const slideItem = document.querySelector(".covers-slide");
-
-    slideItem.style.animation = "none";
+    const slideItem = document.querySelectorAll(".covers-slide");
+    slideItem.forEach((slide) => slide.classList.remove("animation"))
 }
 function startAnimation() {
-    const slideItem = document.querySelector(".covers-slide");
-
-    slideItem.style.animation = 'slideLine 7s infinite';
+    const slideItem = document.querySelectorAll(".covers-slide");
+    slideItem.forEach((slide) => slide.classList.add("animation"));
+    
 }
 
 
@@ -43,12 +31,3 @@ function startAnimation() {
 const observer = new IntersectionObserver(observeFunc, options);
 observer.observe(coverSection)
 
-const slideItem = document.querySelector('.covers-slide');
-const computedStyles = window.getComputedStyle(slideItem);
-const animationName = computedStyles.getPropertyValue('animation-name').trim(); // Отримання ім'я анімації і обрізка пробілів
-
-if (animationName === 'none') {
-  console.log('Анімація зупинена');
-} else {
-  console.log('Анімація працює');
-}
