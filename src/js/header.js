da64e0afd2e678e199182441fb9e197269fa6af3
Vlad <vladswork95@gmail.com>
2024-04-13 11:29:09 +0100

    Update

--- src/js/header.js
+++ src/js/header.js
@@ -1,7 +1,8 @@
 const mobileMenuBtn = document.querySelector(".menu-button");
 const mobileMenu = document.querySelector(".mobile-menu-wrapper");
 const mobileMenuCloseBtn = document.querySelector(".mobile-menu-close-btn");
-
+const tabletAndDesktopMenu = document.querySelector(".tablet-and-desktop-menu-navigation-wrapper");
+const tabletAndDesktopMenuLink = document.querySelector(".menu-link")
 
 mobileMenuBtn.addEventListener("click", () => {
     mobileMenu.classList.add("is-open");
@@ -9,4 +10,13 @@ mobileMenuBtn.addEventListener("click", () => {
 
 mobileMenuCloseBtn.addEventListener("click", () => {
     mobileMenu.classList.remove("is-open");
+})
+
+tabletAndDesktopMenuLink.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (getComputedStyle(tabletAndDesktopMenu).display === 'none') {
+        tabletAndDesktopMenu.style.display = 'block';
+    } else {
+        tabletAndDesktopMenu.style.display = 'none';
+    }
 })
