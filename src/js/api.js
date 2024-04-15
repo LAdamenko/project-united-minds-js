f425cc03bd214868726cbd0ad0a8e34112ea8a63
AnnaSofiya1 <annasofiya1111@gmail.com>
2024-04-15 09:31:20 +0300

    изи

--- src/js/api.js
+++ src/js/api.js
@@ -1,27 +1,32 @@
 import axios from "axios";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
-export function apiGet(){
-    const base_url = "https://portfolio-js.b.goit.study";
+const baseURL = "https://portfolio-js.b.goit.study";
+
+export function apiGet() {
     const end_point = "/api/reviews";
-    const url = `${base_url}${end_point}`;
+    const url = `${baseURL}${end_point}`;
 
     return axios.get(url)
         .then(response => response.data)
-        .catch(error => {
-            console.error(error);
-            throw error;
-        });
+        .catch(handleError);
 }
 
-export function apiPost(data){
-    const base_url = "https://portfolio-js.b.goit.study";
+export function apiPost(data) {
     const end_point = "/api/requests";
-    const url = `${base_url}${end_point}`;
+    const url = `${baseURL}${end_point}`;
 
     return axios.post(url, data)
         .then(response => response.data)
-        .catch(error => {
-            console.error(error);
-            throw error;
-        });
+        .catch(handleError);
+}
+
+function handleError(error) {
+    iziToast.error({
+        title: 'Error!',
+        message: 'Sorry, something went wrong. Please try again.',
+        position: 'topRight',
+    });
+    throw error;
 }
