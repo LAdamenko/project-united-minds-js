668bc2db1a6247ddee2d52d4c0b617b36a28d9c2
LAdamenko <148965340+LAdamenko@users.noreply.github.com>
2024-04-16 23:32:05 +0300

    Revert "Optimization"

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,2 +1,40 @@
-const coverSection = document.querySelector(".covers-section"), options = { root: null, rootMargin: "500px", threshold: .5 }, observeFunc = o => { o.forEach((o => { o.isIntersecting ? startAnimation() : stopAnimation() })) }; function stopAnimation() { document.querySelectorAll(".covers-slide").forEach((o => o.classList.remove("animation"))) } function startAnimation() { document.querySelectorAll(".covers-slide").forEach((o => o.classList.add("animation"))) } const observer = new IntersectionObserver(observeFunc, options); observer.observe(coverSection);
+
+const coverSection = document.querySelector(".covers-section");
+// Об'єкт налаштувань для Observer'а.
+const options = {
+    root: null,
+    rootMargin: "500px",
+    threshold: 0.5,
+}
+
+// Колбек функція для Observer'а.
+const observeFunc = (enteries) => {
+    // Observer створює масив входженнь таргета в зону спостереждення
+    enteries.forEach(entry => {
+        // Коли таргет входить в зону спостереження запускаємо анімацію
+      if (entry.isIntersecting) {
+          startAnimation();
+        //   Коли виходить анімацію зупиняємо
+      } else {
+            stopAnimation();
+      }
+    });
+}
+
+// Функція зупинки анімації
+function stopAnimation() {
+    const slideItem = document.querySelectorAll(".covers-slide");
+    slideItem.forEach((slide) => slide.classList.remove("animation"))
+}
+// Функція запуску анімації
+function startAnimation() {
+    const slideItem = document.querySelectorAll(".covers-slide");
+    slideItem.forEach((slide) => slide.classList.add("animation"));
+    
+}
+// Створення екземпляру Interseption Observer 
+const observer = new IntersectionObserver(observeFunc, options);
+// Передаємо секцію в якості таргета для спостереження 
+observer.observe(coverSection)
+
 
