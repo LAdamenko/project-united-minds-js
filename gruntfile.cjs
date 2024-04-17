99576be4cfdb1323eec91a47e95b85ecc5e96a05
Ladamenko <ladamenko@gmail.com>
2024-04-17 14:18:19 +0300

    Update gruntfile.cjs

--- gruntfile.cjs
+++ gruntfile.cjs
@@ -5,7 +5,19 @@ grunt.initConfig({
     dist: {
       options: {
         base: './'
-      },
+          },
+        dimensions: [{
+          width: 1440,
+          height: 1000,
+        },
+        {
+          width: 768,
+          height: 900,
+        },
+        {
+          width: 375,
+          height: 900,
+        }],
       // The source file
       src: 'src/index.html',
       // The destination file
